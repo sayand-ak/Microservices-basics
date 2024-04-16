cc0d5eae64fa7f4090a7675186bd0f310de63a76
Sayand2002 <sayandak2002@gmail.com>
2024-04-16 11:34:12 +0530

    Completed rabbitmq connection

--- services/product-service/src/controller/productController.js
+++ services/product-service/src/controller/productController.js
@@ -4,7 +4,7 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
     clientId: 'my-app',
-    brokers: ['localhost:9092']
+    brokers: ['kafka-service:9093']
 });
 
 const addProduct = async(req, res, next) => {
